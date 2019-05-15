c641cab80a2c37228085793c261eac63514f6b10
Juan Areces <juan.areces@sendgrid.com>
2019-05-15 11:48:36 -0600

    newDeckFromFile works

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 func main() {
-	cards := newDeck()
-	cards.saveToFile("my_cards")
+	// cards := newDeck()
+	// cards.saveToFile("my_cards")
 
 	// hand, remainingDeck := deal(cards, 5)
 	// hand.print()
 	// remainingDeck.print()
+
+	cards := newDeckFromFile("my_cards")
+	cards.print()
 }
